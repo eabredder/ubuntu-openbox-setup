1891a07e88c6bed659c2d28547b754366fd2b360
eric <eabredder@gmail.com>
2018-04-09 23:03:10 -0400

    .

--- installbash1.sh
+++ installbash1.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-apt install sudo apt install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings openbox obconf obmenu ubuntu-drivers-common mesa-utils-extra compton compton-conf xorg xserver-xorg spacefm guake intel-microcode software-properties-common linux-headers-generic build-essential make -y
-
-reboot
+apt install sudo apt install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings openbox obconf obmenu ubuntu-drivers-common mesa-utils-extra compton compton-conf xorg xserver-xorg spacefm guake intel-microcode software-properties-common linux-headers-generic build-essential make
