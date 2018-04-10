c34fbb4381072e98baa7931915f65972ccac9f2e
eric <eabredder@gmail.com>
2018-04-09 22:59:53 -0400

    .

--- installbash1.sh
+++ installbash1.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-
-#Ubuntu Minimal Install 17.10
-
-#Installs: lightdm, openbox, xorg, compton, spacefm, guake, and more drivers...
 apt install sudo apt install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings openbox obconf obmenu ubuntu-drivers-common mesa-utils-extra compton compton-conf xorg xserver-xorg spacefm guake intel-microcode software-properties-common linux-headers-generic build-essential make -y
 
 reboot
